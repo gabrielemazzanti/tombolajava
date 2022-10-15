aeeda0a66d58ad93905637fff1f1974c20a0d56b
utente <utente@LAIN15>
2022-10-15 11:40:48 +0200

    1

---
+++ src/tombola/Tombola.java
@@ -0,0 +1,16 @@
+package tombola;
+
+/**
+ *
+ * @author utente
+ */
+public class Tombola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
